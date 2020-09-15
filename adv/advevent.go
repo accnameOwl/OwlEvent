5ae5b031eb5ed60e07bcb3f457a1e0115eeba5af
Tor-Arne <Torarnefe94@gmail.com>
2020-09-15 23:11:08 +0200

    mutual exclusion added...

--- adv/advevent.go
+++ adv/advevent.go
@@ -1,14 +1,28 @@
 package adv
 
 import (
-	"errors"
-	"runtime/debug"
+	"sync"
 )
+
+// TODO : Impl Mutex lock... Examples:
+//		Mutex lock/Unlock:
+// 		-> https://tour.golang.org/concurrency/9
+//		Defer panic and recover, instead of concurrent locking:
+//		-> https://blog.golang.org/defer-panic-and-recover
 
 // EventHandler ...
 type EventHandler interface {
-	Id(ev *Event) (string, error)
 	Callback(ev *Event) error
+	RemoveEventByIndex(index int)
+	AppendEvents(move *[]Event, to *[]Event)
+	Push(push *Event, to *[]Event)
+	PopAndGetEvent(index int, el *[]Event) (event Event)
+}
+
+// SafeAsyncEvents ...
+type SafeAsyncEvents struct {
+	events []Event
+	mux    sync.Mutex
 }
 
 // Event ...
@@ -21,25 +35,33 @@ type Event struct {
 
 // RemoveEventByIndex ...
 // -> RemoveEventByIndex(indexToRemove, &EventList)
-func RemoveEventByIndex(index int, el *[]Event) {
-	(*el) = append((*el)[:index], (*el)[index+1:]...)
+func (sae *SafeAsyncEvents) RemoveEventByIndex(index int) {
+	sae.mux.Lock()
+	defer sae.mux.Unlock()
+	sae.events = append(sae.events[:index], sae.events[index+1:]...)
 }
 
 // AppendEvents ...
-func AppendEvents(move *[]Event, to *[]Event) {
-	(*to) = append((*to), (*move)...)
+func (sae *SafeAsyncEvents) AppendEvents(move *[]Event) {
+	sae.mux.Lock()
+	defer sae.mux.Unlock()
+	sae.events = append(sae.events, (*move)...)
 }
 
 // Push ...
-func Push(push *Event, to *[]Event) {
-	(*to) = append((*to), *push)
+func (sae *SafeAsyncEvents) Push(push *Event, to *[]Event) {
+	sae.mux.Lock()
+	defer sae.mux.Unlock()
+	sae.events = append(sae.events, *push)
 }
 
-// PopAndGetEvent ...
+// PopAndGet ...
 // Pop EventList[index], and return EventList[index]
 //
 // EventList gets shifted on Pop
-func PopAndGetEvent(index int, el *[]Event) (event Event) {
-	event, (*el) = (*el)[index], append((*el)[:index], (*el)[index+1:]...)
+func (sae *SafeAsyncEvents) PopAndGet(index int, el *[]Event) (event Event) {
+	sae.mux.Lock()
+	defer sae.mux.Unlock()
+	event, sae.events = sae.events[index], append(sae.events[:index], sae.events[index+1:]...)
 	return event
 }
