714a5430349a46d41f01a603102bf8bf1f94f7c9
Tor-Arne <Torarnefe94@gmail.com>
2020-09-15 22:01:16 +0200

    Advanced event functionality

--- adv/advevent.go
+++ adv/advevent.go
@@ -21,53 +21,25 @@ type Event struct {
 
 // RemoveEventByIndex ...
 // -> RemoveEventByIndex(indexToRemove, &EventList)
-func RemoveEventByIndex(index int, el *[]Event) (err error) {
-	if (index >= 0) || len((*el)) >= 0 {
-		(*el) = append((*el)[:index], (*el)[index+1:]...)
-	} else {
-		err = errors.New("length of argument \"el *[]Event\" not established")
-		debug.PrintStack()
-	}
-	return err
+func RemoveEventByIndex(index int, el *[]Event) {
+	(*el) = append((*el)[:index], (*el)[index+1:]...)
 }
 
 // AppendEvents ...
-func AppendEvents(move *[]Event, to *[]Event) (err error) {
-	if len((*move)) >= 0 {
-		if len((*to)) >= 0 {
-			(*to) = append((*to), (*move)...)
-		} else {
-			err = errors.New("Could not establish Event slice to append Event slice to")
-			debug.PrintStack()
-		}
-	} else {
-		err = errors.New("Could not establish Event slice to append")
-		debug.PrintStack()
-	}
-	return err
+func AppendEvents(move *[]Event, to *[]Event) {
+	(*to) = append((*to), (*move)...)
 }
 
 // Push ...
-func Push(push *Event, to *[]Event) (err error) {
-	if len((*to)) >= 0 {
-		(*to) = append((*to), *push)
-	} else {
-		err = errors.New("Could not establish Event slice to push Event to")
-		debug.PrintStack()
-	}
-	return err
+func Push(push *Event, to *[]Event) {
+	(*to) = append((*to), *push)
 }
 
 // PopAndGetEvent ...
 // Pop EventList[index], and return EventList[index]
 //
 // EventList gets shifted on Pop
-func PopAndGetEvent(index int, el *[]Event) (event Event, err error) {
-	if index < len((*el)) {
-		event, (*el) = (*el)[index], append((*el)[:index], (*el)[index+1:]...)
-	} else {
-		err = errors.New("index out of bounds of []Event argument")
-		debug.PrintStack()
-	}
-	return event, err
+func PopAndGetEvent(index int, el *[]Event) (event Event) {
+	event, (*el) = (*el)[index], append((*el)[:index], (*el)[index+1:]...)
+	return event
 }
